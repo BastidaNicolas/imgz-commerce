a4f813610d134eb0b7c7ad43728ed1a382163b97
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-04-17 20:41:49 -0300

    responsive header

--- src/components/buttons/clearBtn.tsx
+++ src/components/buttons/clearBtn.tsx
@@ -1,6 +1,6 @@
 export default function ClearBtn({setState, value}:any) {
     return (
-      <button onClick={() => setState(value)} className='w-full text-lg text-black bg-white py-2.5 px-8 font-medium  border-[3px] border-black'>
+      <button onClick={() => setState(value)} className='w-full text-lg text-black bg-white py-2.5 px-8 font-medium  border-[3px] border-black truncate'>
         CLEAR
       </button>
     )
--- src/components/cards/cardSm.tsx
+++ src/components/cards/cardSm.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function CardSm() {
   return (
     <div className="group flex justify-between items-center w-full">
-      <div className="" >
+      <div className="truncate m-1" >
         <div className="text-xl text-black font-bold mb-2.5 truncate">Samurai King Resting</div>
-        <div className="text-3xl text-neutral-600">$100.00</div>
+        <div className="text-3xl text-neutral-600 truncate">$100.00</div>
       </div>
       <div className="relative h-[5.375rem] max-w-[9.313rem] w-full">
         <Image
