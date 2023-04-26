e2f35884b7a3dca8e3badd4f3ca0f4e33bd63162
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-04-26 12:48:20 -0300

    better SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <title> Photo Print On Aluminum Backing Shop</title>
-      <Head />
+      <Head>
+        <title>Aluminum-backed Photo Printing Shop</title>
+        <meta
+          name="description"
+          content="Shop for stunning photo prints on aluminum backing at our online store. Browse our selection of high-quality photos and purchase your favorites. Free shipping on all orders"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
