34e5d4ece4febf9ff07e94dae313c4b2a5167667
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-04-26 19:53:49 -0300

    title fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,8 @@ export default function Document() {
     <Html lang="en">
       <title>Buy aluminum backed photo printings in this shop</title>
       <Head>
+        <title>Buy aluminum backed photo printings in this shop</title>
+
         <meta
           name="description"
           content="Shop for stunning photo prints on aluminum backing at our online store. Browse our selection of high-quality photos and purchase your favorites. Free shipping on all orders"
