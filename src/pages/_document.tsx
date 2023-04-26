8213de31b07e300e71e255324e3b6f3cfa38d4ec
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-04-26 20:04:32 -0300

    title fix 4

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <body>
-        <Head>
+      <div>
+      <Head>
           <title>Buy aluminum backed photo printings in this shop</title>
           {/* <meta property="og:title" content="Buy aluminum backed photo printings in this shop" key="title" /> */}
           <meta
@@ -12,6 +12,8 @@ export default function Document() {
             content="Shop for stunning photo prints on aluminum backing at our online store. Browse our selection of high-quality photos and purchase your favorites. Free shipping on all orders"
           />
         </Head>
+      </div>
+      <body>
         <Main />
         <NextScript />
       </body>
