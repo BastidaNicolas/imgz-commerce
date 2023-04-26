f7d617456ee82a77804d05744a5827cc7aad3d3f
Nic <74965310+BastidaNicolas@users.noreply.github.com>
2023-04-26 20:11:35 -0300

    removed description meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -69,10 +69,7 @@ export default function Home() {
       <Head>
           <title>Buy aluminum backed photo printings in this shop</title>
           {/* <meta property="og:title" content="Buy aluminum backed photo printings in this shop" key="title" /> */}
-          <meta
-            name="description"
-            content="Shop for stunning photo prints on aluminum backing at our online store. Browse our selection of high-quality photos and purchase your favorites. Free shipping on all orders"
-          />
+      
       </Head>
       <TotalCartItemsContext.Provider value={totalCartItemMemo}>
         <Header></Header>
